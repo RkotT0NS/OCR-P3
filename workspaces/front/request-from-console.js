d009b1e6d32fd15b9ad828ac2ed8caeec22811e2
Fabien Jacq <tetedacier@proton.me>
2025-12-15 10:34:35 +0100

    Spawn a vite frontend in a dedicated workspace

---
+++ workspaces/front/request-from-console.js
@@ -0,0 +1,73 @@
+
+async function getTags() {
+    return fetch('/api/tags', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(res => {
+            console.log("Status:", res.status);
+            return res.json();
+        })
+        .then(data => console.log(data));
+}
+
+/**
+ *
+ * @param {{name:string}} newTag
+ * @returns
+ */
+async function storeTag(newTag) {
+    return fetch('/api/tags', {
+        method: 'POST',
+        body: JSON.stringify(newTag),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        }
+    })
+        .then(res => {
+            console.log("Status:", res.status); // Should now be 200 or 401
+            Livewire.dispatch('tagCreated');
+            return res.json();
+        })
+        .then(data => console.log(data));
+}
+
+async function getNotes() {
+    return fetch('/api/notes', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(res => {
+            console.log("Status:", res.status);
+            return res.json();
+        })
+        .then(data => console.log(data));
+}
+
+/**
+ *
+ * @param {{content:string, tag: number}} newNote
+ * @returns
+ */
+async function storeNote(newNote) {
+    return fetch('/api/notes', {
+        method: 'POST',
+        body: JSON.stringify(newNote),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        }
+    })
+        .then(res => {
+            console.log("Status:", res.status); // Should now be 200 or 401
+            return res.json();
+        })
+        .then(data => console.log(data));
+}
